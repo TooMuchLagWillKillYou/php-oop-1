ff1d8fbea2277ca3600c20415d98157b2a55af04
Diego Barbaresco <diego.bar@live.it>
2021-05-20 08:59:02 +0200

    esercizio corretto

--- MyLaravel/app/Http/Controllers/MyController.php
+++ MyLaravel/app/Http/Controllers/MyController.php
@@ -9,9 +9,10 @@ class Movies {
     public $title;
     public $description;
 
-    public function __construct($title){
+    public function __construct($title, $description = 'none'){
 
         $this->title = $title;
+        $this->description = $description;
     }
 
     public function getString(){
@@ -26,12 +27,9 @@ class MyController extends Controller
 
         $movie1 = new Movies('No Country for Old Men');
         $movie1->description = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur modi cumque totam quaerat expedita facilis cupiditate culpa, ipsa ullam velit aliquid aperiam distinctio. Maiores vel amet libero similique consequuntur repellat!';
-
         $movie1Str = $movie1->getString();
 
         $movie2 = new Movies('Forrest Gump');
-        $movie2->description = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur modi cumque totam quaerat expedita facilis cupiditate culpa, ipsa ullam velit aliquid aperiam distinctio. Maiores vel amet libero similique consequuntur repellat!';
-        
         $movie2Str = $movie2->getString();
 
         dd($movie1Str, $movie2Str);
